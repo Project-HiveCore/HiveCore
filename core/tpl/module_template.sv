e22da2218dce913c10aad3c0e787fa8d3080ee98
Jacob Dudik <jdudik03@gmail.com>
2026-02-21 16:12:08 -0500

    Add SystemVerilog module template.

---
+++ core/tpl/module_template.sv
@@ -0,0 +1,32 @@
+/********************************************************
+* Module Name    : <module_name>
+* Author         : 
+* Creation Date  : 
+* Last edit Date : 
+* Description    : 
+*                  
+*                  
+********************************************************/ 
+
+`ifndef <MODULE_NAME>
+`define <MODULE_NAME>
+
+module <module_name>
+    #(
+        // ======= Parameters ========
+
+    )
+    (
+        // ========= Inputs ==========
+
+        // ========= Outputs =========
+
+    );
+
+    // ============================================
+    // ==            Blank Header                ==
+    // ============================================
+
+    
+endmodule
+`endif
