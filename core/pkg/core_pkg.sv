2719f9b457a42cbe74cfea03acb97873810d3a4d
Jacob Dudik <jdudik03@gmail.com>
2026-08-07 02:18:08 -0400

    Update opcode names and shift direction enum

--- core/pkg/core_pkg.sv
+++ core/pkg/core_pkg.sv
@@ -14,21 +14,36 @@ parameter INT_REG_WIDTH         = 64;   // Register data width (XLEN = 64)
 parameter INT_MAX_IMM_WIDTH     = 20;   // Max width of an immediate value in an instruction
                                         // - Not all immediate values are this length (ex. I-type imm == 12-bits)
 
+// Shift Direction
+typedef enum logic {
+  LEFT  = 1'b0,
+  RIGHT = 1'b1
+} shift_dir_e;
+
 // OPCODES
 localparam OP_W = 7;
 typedef enum logic [OP_W-1:0] {
   //  I-Extension Opcodes
-  OP_LOAD   = 7'b0000011, // I-type (load)
-  OP_REGIMM = 7'b0010011, // I-type (register-immediate)
-  OP_AUIPC  = 7'b0010111, // U-type (add upper immediate to pc)
-  OP_STORE  = 7'b0100011, // S-type
-  OP_REG    = 7'b0110011, // R-type
-  OP_LUI    = 7'b0110111, // U-type (load upper immediate)
-  OP_BRANCH = 7'b1100011, // B-type
-  OP_JALR   = 7'b1100111, // I-type (jump and link register)
-  OP_JAL    = 7'b1101111, // J-type
-  OP_ENV    = 7'b1110011  // I-type (environment call/break)
-} opcode_t;
+  OP        = 7'b01_100_11, // R-type (Reg-Reg w/ 64-bit)
+  OP_32     = 7'b01_110_11, // R-type (Reg-Reg w/ 32-bit)
+  
+  OP_IMM    = 7'b00_100_11, // I-type (Reg-Immediate 64-bit)
+  OP_IMM_32 = 7'b00_110_11, // I-type (Reg-Immediate 32-bit) 
+
+  LOAD      = 7'b00_000_11, // I-type (load)
+  STORE     = 7'b01_000_11, // S-type
+
+  BRANCH    = 7'b11_000_11, // B-type
+ 
+  JAL       = 7'b11_011_11, // J-type
+  JALR      = 7'b11_001_11, // I-type (jump and link register)
+
+  LUI       = 7'b01_101_11, // U-type (load upper immediate)
+  AUIPC     = 7'b00_101_11, // U-type (add upper immediate to pc)
+
+  SYSTEM    = 7'b11_100_11, // I-type (environment call/break)
+  MISC_MEM  = 7'b00_011_11
+} opcode_e;
 
 // FUNCTION 3
 typedef enum logic [2:0] {
@@ -40,7 +55,7 @@ typedef enum logic [2:0] {
   ALU_SRL_SRA = 3'b101,
   ALU_OR      = 3'b110,
   ALU_AND     = 3'b111
-} alu_funct3_t;
+} alu_funct3_e;
 
 typedef enum logic [2:0] {
   LSU_LB_SB   = 3'h0,
@@ -48,7 +63,7 @@ typedef enum logic [2:0] {
   LSU_LW_SW   = 3'h2,
   LSU_LBU     = 3'h4,
   LSU_LHU     = 3'h5
-} lsu_funct3_t;
+} lsu_funct3_e;
 
 typedef enum logic [2:0] {
   BEQ         = 3'h0,
@@ -57,16 +72,19 @@ typedef enum logic [2:0] {
   BGE         = 3'h5,
   BLTU        = 3'h6,
   BGEU        = 3'h7
-} branch_funct3_t;
+} branch_funct3_e;
 
 // Functional Unit Identifiers
 typedef enum logic [2:0] {
+  NONE,
   ALU,
-  LSU,
+  LOAD,
+  STORE,
   BRANCH,
   MUL,
-  DIV
-} func_unit_t;
+  DIV,
+  SYS
+} func_unit_e;
 
 endpackage
 `endif
