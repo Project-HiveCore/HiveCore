056edf82f83b169222b87691467293775b101450
Jacob Dudik <jdudik03@gmail.com>
2026-08-16 16:02:29 -0400

    Decode execute interface comments

--- core/pkg/interface_struct_pkg.sv
+++ core/pkg/interface_struct_pkg.sv
@@ -3,16 +3,16 @@
 package interface_struct_pkg;
 
 typedef struct packed {
-  logic [MEM_ADDR_WIDTH-1:0]    pc;
-  func_unit_e                   fu;
-  logic [3:0]                   uOP;
-  logic [4:0]                   rs1_addr;
-  logic [INT_REG_WIDTH-1:0]     rs1_data;
-  logic [4:0]                   rs2_addr;
-  logic [INT_REG_WIDTH-1:0]     rs2_data;
-  logic [INT_MAX_IMM_WIDTH-1:0] imm;
-  logic                         is_32b;
-  logic                         en_wb;      // Enable the writeback stage
+  logic [MEM_ADDR_WIDTH-1:0]    pc;       // Program Counter
+  func_unit_e                   fu;       // Functional Unit
+  logic [3:0]                   uOP;      // Micro-OP for FUs
+  logic [4:0]                   rs1_addr; // rs1 address
+  logic [INT_REG_WIDTH-1:0]     rs1_data; // rs1 data from regfile
+  logic [4:0]                   rs2_addr; // rs2 address
+  logic [INT_REG_WIDTH-1:0]     rs2_data; // rs2 data from regfile
+  logic [INT_MAX_IMM_WIDTH-1:0] imm;      // Selected immediate value
+  logic                         is_32b;   // Instruction output is 32-bit
+  logic                         en_wb;    // Writeback stage enable
 } decode_execute_if_t;
 
 endpackage
